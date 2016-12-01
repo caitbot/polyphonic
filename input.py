a6f2a7058f935a38b35d7a1bf32446643cc12d5b
Adam Grupa <adam.grupa@gmail.com>
2016-12-01 05:35:55 -0500

    Finish pipeline

--- input.py
+++ input.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 
 current_batch = 0
+eval_batch = 0
 
 data_path = '/mnt/hgfs/link/'
 insts = sorted(os.listdir(data_path))
@@ -25,6 +26,7 @@ def input():
     return (np.asarray(samples), np.asarray(labels))
     
 def get_batch(size):
+    
     global current_batch
     next_batch = current_batch + size
     samples = []
@@ -32,7 +34,15 @@ def get_batch(size):
 
 	#gets data from wave files
     for instrument in insts:
-		for song in os.listdir(data_path + instrument)[current_batch:next_batch]:
+        songs = os.listdir(data_path + instrument)
+        
+        if len(songs[current_batch:]) == 0:
+            skip
+        
+        if len(songs[current_batch:]) < size:
+            next_batch = current_batch + len(songs[current_batch:])
+        
+        for song in songs[current_batch:next_batch]:
 		    data, samplerate = sf.read(data_path + instrument + "/" + song)
 		    
 		    samples.append(data);
@@ -43,4 +53,34 @@ def get_batch(size):
     current_batch = next_batch
 
     return (samples, np.asarray(labels))
+    
+def eval_get_batch(size):
+    
+    global eval_batch
+    next_batch = eval_batch + size
+    samples = []
+    labels = []
+
+	#gets data from wave files
+    for instrument in insts:
+        songs = os.listdir(data_path + instrument)
+        
+        if len(songs[eval_batch:]) == 0:
+            skip
+        
+        if len(songs[eval_batch:]) < size:
+            next_batch = eval_batch + len(songs[eval_batch:])
+        
+        for song in songs[eval_batch:next_batch]:
+		    data, samplerate = sf.read(data_path + instrument + "/" + song)
+		    
+		    samples.append(data);
+		    ohv = [0] * 11
+		    ohv[insts.index(instrument)] = 1
+		    labels.append(ohv)
+		    
+    current_batch = next_batch
+
+    return (samples, np.asarray(labels))
+
 
--- train.py
+++ train.py
@@ -1,60 +1,139 @@
 import tensorflow as tf
 import math
+import time
 
 def conv1d(x, f):
-    return tf.nn.conv1d(x, f, strides=[1,1,1,1], padding='SAME')
+    return tf.nn.conv1d(x, f, stride=1, padding='SAME')
 
 def max_pool(x):
-    return tf.nn.max_pool(p, ksize=[1, 3, 1, 1], strides=[1, 3, 1, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 1, 3, 1], strides=[1, 1, 3, 1], padding='SAME')
 
 def inference(data):
     
     with tf.name_scope('conv1'):
-        filter_weights = tf.Variable(tf.random_normal([3, 2, 3], stddev=1.0, name="filter_weights"))
-        conv1 = tf.nn.relu(conv1d(data, f))
+        filter_weights = tf.Variable(tf.random_normal([3, 2, 10], name='filter_weights'))
+        conv1 = tf.nn.relu(conv1d(data, filter_weights))
         
     with tf.name_scope('max1'):
-        max1 = tf.nn.max_pool(tf.reshape(conv1, [-1, 1, 132299, 3]), [1, 2, 1, 1], [1, 2, 1, 1], 'SAME')
+        max1 = max_pool(tf.reshape(conv1, [-1, 1, 132299, 10]))
         
     with tf.name_scope('conv2'):
-        filter_weights = tf.Variable(tf.random_normal([3, 2, 3], stddev=1.0, name="filter_weights"))
-        conv2 = tf.nn.relu(conv1d(max1, f))
+        filter_weights = tf.Variable(tf.random_normal([3, 10, 20], name='filter_weights'))
+        conv2 = tf.nn.relu(conv1d(tf.reshape(max1, [-1, 44100, 10]), filter_weights))
         
     with tf.name_scope('max2'):
-        max2 = tf.nn.max_pool(tf.reshape(conv2, [-1, 1, 132299, 3]), [1, 2, 1, 1], [1, 2, 1, 1], 'SAME')
+        max2 = max_pool(tf.reshape(conv2, [-1, 1, 44100, 20]))
         
     with tf.name_scope('logits'):
-        logits = tf.matmul(tf.reshape(m, [-1, 132299 * 3]), max2)
+        weights = tf.Variable(tf.random_normal([14700 * 20, 11]), name='weights')
+        logits = tf.matmul(tf.reshape(max2, [-1, 14700 * 20]), weights, name='logits')
         
     return logits
+    
+def loss(logits, labels):
 
-def init_graph():
-    len([1])
+    with tf.name_scope('loss'):
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, labels, name='cross_entropy')
+        
+        loss = tf.reduce_mean(cross_entropy, name='cross_entropy_mean')
+        
+    return loss
+        
+def training(loss):
+    
+    tf.scalar_summary('loss', loss)
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    
+    with tf.name_scope('training'):
+        optimizer = tf.train.GradientDescentOptimizer(0.05)
+        
+        train_op = optimizer.minimize(loss, global_step=global_step)
+        
+    return train_op
+    
+def evaluation(logits, labels):
+    
+    with tf.name_scope('evaluation'):
+        correct = tf.equal(tf.argmax(logits,1), tf.argmax(labels,1))
+    
+    return tf.reduce_sum(tf.cast(correct, tf.int32))
+    
+def placeholder_inputs():
+    
+    data_placeholder = tf.placeholder(tf.float32, shape=[None, 132299, 2], name='data')
+    
+    labels_placeholder = tf.placeholder(tf.int32, shape=[None, 11], name='labels')
 
-def train(sounds, labels):
-    tf.reset_default_graph()
-    a = tf.placeholder(tf.float32, [None, 132299, 2])
-    l = tf.placeholder(tf.float32, [None, 11])
-    f = tf.Variable(tf.random_normal([3, 2, 3], dtype=tf.float32))
-    w = tf.Variable(tf.random_normal([132299 * 3, 11]), dtype=tf.float32)
+    return data_placeholder, labels_placeholder
     
-    t = tf.nn.conv1d(a, f, 1, 'SAME')
-    r = tf.nn.relu(t)
-    p = tf.reshape(r, [10, 1, -1, 3])
-    m = tf.nn.max_pool(p, [1, 2, 1, 1], [1, 2, 1, 1], 'SAME')
-    print m
-    mul = tf.matmul(tf.reshape(m, [-1, 132299 * 3]), w)
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(mul, l))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    print mul
-    correct_prediction = tf.equal(tf.argmax(mul,1), tf.argmax(l,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+def fill_feed_dict(batch_size, data_pl, labels_pl, eval=False):
     
-    sess = tf.Session()
-    sess.run(tf.initialize_all_variables())
+    batch = []
     
-    for i in range(20):
-        batch = get_batch(10)
-        print sess.run(f)
-        print sess.run(train_step, feed_dict={a:batch[0], l:batch[1]})
-        print sess.run(accuracy, feed_dict={a:batch[0], l:batch[1]})
+    if eval:
+        batch = eval_get_batch(batch_size)
+    else:
+        batch = get_batch(batch_size)
+    
+    feed_dict = {data_pl: batch[0], labels_pl: batch[1]}
+    
+    return feed_dict
+    
+def do_eval(sess, eval_correct, data_pl, labels_pl, batch_size):
+    
+    true_count = 0
+    
+    for step in range(30):
+        feed_dict = fill_feed_dict(batch_size, data_pl, labels_pl, True)
+    
+        true_count += sess.run(eval_correct, feed_dict=feed_dict)
+        
+    precision = true_count / 300
+    
+    print 'Num examples: 300 Num correct: {} Precision: {}'.format(true_count, precision)
+    
+def run_training():
+    
+    with tf.Graph().as_default():
+    
+        data_placeholder, labels_placeholder = placeholder_inputs()
+        
+        logits = inference(data_placeholder)
+        
+        loss_op = loss(logits, labels_placeholder)
+        
+        train_op = training(loss_op)
+        
+        eval_correct = evaluation(logits, labels_placeholder)
+        
+        summary = tf.merge_all_summaries()
+        
+        init = tf.initialize_all_variables()
+        
+        sess = tf.Session()
+        
+        summary_writer = tf.train.SummaryWriter('.\summ', sess.graph)
+        
+        sess.run(init)
+
+        
+        for step in range(10):
+            start_time = time.time()
+            
+            feed_dict = fill_feed_dict(10, data_placeholder, labels_placeholder)
+            
+            _, loss_value = sess.run([train_op, loss_op], feed_dict=feed_dict)
+            
+            duration = time.time() - start_time
+            
+            print 'Step {}: loss = {} ({} sec)'.format(step, loss_value, duration)
+            
+            summary_str = sess.run(summary, feed_dict=feed_dict)
+            summary_writer.add_summary(summary_str, step)
+            summary_writer.flush()
+            
+            print 'Training Data Eval:'
+            if (step + 1) % 5 == 0:
+                do_eval(sess, eval_correct, data_placeholder, labels_placeholder, 10)
+
+
